d2cd8d2e767b5227baf20c7b0d2b4910819d8755
Carfer10 <fernando.fvb.fvb@gmail.com>
2019-01-20 21:55:48 +0100

    Minor change

---
+++ flexcup/Env/Program.java
@@ -0,0 +1,25 @@
+package Env;
+import java.util.Scanner;
+public class Program {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        MachineController mc = new MachineController(Machines.automata1());
+
+
+        System.out.print("Input: ");
+        String input = sc.nextLine();
+
+        while(!input.equals("")){
+            if (mc.addNewInput(input) != null){
+                mc.getCurrentState().getOutput().accept(mc.getEnvironment());
+            }
+            else{
+                System.out.println("Entrada no reconocida por el automata");
+            }
+            System.out.print("Input: ");
+            input = sc.nextLine();
+        }
+
+        sc.close();
+    }
+}
